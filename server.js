cc2e7b8566a73344deb11e26699decf9a319fd93
Ryan Trontz <rtrontz@gmail.com>
2019-01-22 11:12:28 -0800

    added logging for requests to the recipes endpoint

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ const express = require('express');
 const app = express();
 const port = pocess.env.PORT || process.argv[2] || 8000;
 
+app.use(bodyParser.json());
+
+app.get('/', /* cb */)
+
+app.get('/recipes', (req, res) => {
+  console.log(`get request to recipes`);
+})
+app.post('/recipes', (req, res) => {
+  console.log(`post request to recipes`);
+})
+app.get('/recipes/:id', (req, res) => {
+  console.log(`get request to recipes/${req.params.id}`);
+})
+app.put('/recipes/:id', (req, res) => {
+  console.log(`put request to recipes/${req.params.id}`);
+})
+app.delete('/recipes/:id', (req, res) => {
+  console.log(`delete request to recrecipes/${req.params.id}ipes`);
+})
+
 
 
 http.createServer(app).listen(PORT, () => console.log(`we are listening for requests on port ${port}`));
